9df2ab9e54f8fbf9e2ad8451a52c28687905ce32
cinesedigital <contas@cinesedigital.com.br>
2023-07-07 18:24:42 -0300

    mais um

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -4,12 +4,12 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: 'Daniela',
-      email: 'dani@live.com',
-    },
-  });
+  // const newUser = await prisma.user.create({
+  //   data: {
+  //     name: 'Daniela',
+  //     email: 'dani@live.com',
+  //   },
+  // });
  
   const users = await prisma.user.findMany();
  
