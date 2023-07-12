33b5fb60af37228dcadc84cc9dfc6e50df1392dc
cinesedigital <contas@cinesedigital.com.br>
2023-07-12 18:52:44 -0300

    teste do cache

--- src/app/api/scripts/teste/route.ts
+++ src/app/api/scripts/teste/route.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { NextResponse } from 'next/server';
 
-export const revalidate = 10;
+
 
 export async function GET() {
     const filePath = path.join(process.cwd(), 'src', 'scripts', 'teste.js');
@@ -14,3 +14,5 @@ export async function GET() {
         },
     });
 }
+
+export const revalidate = 'force-cache'
